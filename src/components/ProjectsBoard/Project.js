57c4648ac985d11bb28d4a5dd336f3ab7bd1c9bb
nicoletatum <nicoletatum@outlook.com>
2021-03-17 21:38:44 -0500

    project cards show on project board but when i try to only show projects w id matching ss id it breaks

--- src/components/ProjectsBoard/Project.js
+++ src/components/ProjectsBoard/Project.js
@@ -1,13 +1,9 @@
 //created by nicole 
-import React from "react"
+import React, { useContext, useState } from "react"
 // import "./Project.css"
-// import { ProjectContext } from "./ProjectProvider.js"
-// import { useHistory } from "react-router-dom"
+import { ProjectContext } from "./ProjectProvider.js"
 
 export const ProjectCard = ({ project }) => {
-
-    // const history = useHistory();
-    // const { getProjects } = useContext(ProjectContext)
     return (
             <section className="ProjectCard">
             <h3 className="projectName">{project.name}</h3>
--- src/components/ProjectsBoard/ProjectBoard.js
+++ src/components/ProjectsBoard/ProjectBoard.js
@@ -1,35 +1,38 @@
 import { useContext, useEffect } from "react"
 import { ProjectContext } from "./ProjectProvider.js"
 import { ProjectCard } from "./Project.js"
-// import { useHistory } from "react-router"
-// import "./Project.css"
-// import { useHistory } from "react-router-dom"
+import { userStorageKey } from "../auth/authSettings.js"
+
 
 export const ProjectBoard = () => {
     const { projects, getProjects } = useContext(ProjectContext)
 
-    let currentUser = parseInt(sessionStorage.getItem("app_user_id"))
+    // let currentUser = parseInt(sessionStorage.getItem(userStorageKey))
+    
     useEffect(() => {
         getProjects()
     }, [])
 
-    let userProjects = projects.filter(project => currentUser === project.userId)
+    // let userProjects = projects.filter(project => currentUser === project.userId)
     // const history = useHistory()
 
     return (
         <>
             <h3 className="projectHeader"> <p>projects</p> </h3>
             <div className="projects">
-            {userProjects.map(project => {
-                return <ProjectCard /> 
+            {
+                projects.map(project => {
+                return <ProjectCard key={project.id}
+                            project={project} /> 
                 })
             }
             </div>
             <div className="projectButtonDiv">
-                <button className="addProjectButton">
+            <button className="addProjectButton">
                     Add Project
             </button>
             </div>
         </>
     )
 }
+console.log(ProjectCard)
