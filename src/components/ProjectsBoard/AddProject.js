42edecf906d1119ce420302276c1d5392b0e845d
nicoletatum <nicoletatum@outlook.com>
2021-03-18 14:01:05 -0500

    expanded category oops

--- src/components/ProjectsBoard/AddProject.js
+++ src/components/ProjectsBoard/AddProject.js
@@ -1,6 +1,38 @@
-// import React, { useContext, useEffect, useState } from "react"
-// import { useHistory } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react"
+import { useHistory } from 'react-router-dom';
+import { ProjectContext } from "./ProjectProvider"
 
-// export const CreateProject = () => {
+export const CreateProject = () => {
 
-// }
+    const { addProject } = useContext(ProjectContext)
+
+    const [project, setProject] = useState({
+        name:"",
+        category:"",
+        description: "",
+        userId: 0,
+        categoryId: 0,
+        dateCreated: "",
+        dateDue: ""
+    })
+
+    const history = useHistory();
+
+    // useEffect(() => {
+    //     getProjects()
+    // }, [])
+
+    // const
+
+    return(
+        <form className="ProjectCard">
+            <h3 className="projectName">{project.name}</h3>
+            <div className="projectDescription">{project.description}</div>
+            <div className="projectUser">{project.userId}</div>
+            <div className="projectcategory">{project.categoryId}</div>
+            <div className="projectCreationDate">{project.dateCreated}</div>
+            <div className="projectCompletionDate">Complete by: {project.dueDate}</div>
+            <button > edit </button>
+        </form>
+    )
+}
--- src/components/ProjectsBoard/Project.js
+++ src/components/ProjectsBoard/Project.js
@@ -7,12 +7,13 @@ export const ProjectCard = ({ project }) => {
     return (
             <section className="ProjectCard">
             <h3 className="projectName">{project.name}</h3>
-            <div className="projectDescription">{project.description}</div>
-            <div className="projectUser">{project.userId}</div>
-            <div className="projectcategory">{project.categoryId}</div>
-            <div className="projectCreationDate">{project.dateCreated}</div>
-            <div className="projectCompletionDate">Complete by: {project.dueDate}</div>
+            <div className="projectDescription">Description: {project.description}</div>
+            {/* <div className="projectUser">{project.user.name}</div> */}
+            <div className="projectcategory">Category: {project.category.name}</div>
+            <div className="projectCreationDate">Date Started: {project.dateCreated}</div>
+            <div className="projectCompletionDate">Complete by: {project.dateDue}</div>
             <button > edit </button>
+            <button > delete </button>
         </section>
     )
 }
