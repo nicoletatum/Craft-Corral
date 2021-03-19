e31434c36177a5f2a8460acc543e01dae31844ad
nicoletatum <nicoletatum@outlook.com>
2021-03-18 21:27:07 -0500

    bootstap messing around

--- src/components/ProjectsBoard/AddProject.js
+++ src/components/ProjectsBoard/AddProject.js
@@ -2,24 +2,27 @@ import React, { useContext, useEffect, useState } from "react"
 import { useHistory } from 'react-router-dom';
 import { ProjectContext } from "./ProjectProvider"
 import { userStorageKey } from "../auth/authSettings"
-import { Form, FormLabel, Button } from "react-bootstrap";
+import { Form, FormLabel, Button, Jumbotron } from "react-bootstrap";
+import "./ProjectBoard.css"
 
 export const CreateProject = () => {
+    const { addProject } = useContext(ProjectContext)
+    
     let currentUser = parseInt(sessionStorage.getItem(userStorageKey))
-    const history = useHistory();
-
-    // const { addProject } = useContext(ProjectContext)
-
-
+    const timestamp = new Date().toLocaleString()
+    
     const [project, setProject] = useState({
         name:"",
-        category:"",
+        categoryId:"",
         description: "",
         userId: currentUser,
         categoryId: 0,
-        dateCreated: "",
+        dateCreated: timestamp,
         dateDue: ""
     })
+
+    const history = useHistory();
+
     const handleControlledInputChange = (event) => {
         const newProject = { ...project}
         let selectedVal = event.target.value
@@ -38,24 +41,55 @@ export const CreateProject = () => {
 
     return(
         <>
-        <Form className="ProjectForm">
+        <Jumbotron className="projectsTitle"> <h3>Project Form</h3> </Jumbotron>
+        <Form className="projectForm">
             <Form.Group>
                 <Form.Label id="project.name"> Project Name: </Form.Label>
                 <Form.Control type="text" placeholder="Enter name of project" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label id="project.categoryId"> Project Category: </Form.Label>
+                <Form.Control as="select" custom>
+                <option></option>
+                </Form.Control>
             </Form.Group>
             <Form.Group>
                 <Form.Label id="project.description"> Project Description: </Form.Label>
                 <Form.Control type="text" placeholder="Tell Crusty a bit about your project" />
             </Form.Group>
             <Form.Group>
-                <Form.Label id="project.categoryId"> Project Category: </Form.Label>
-                <Form.Control type="text" placeholder="Select Category " />
+                <Form.Label id="project.tools"> Tools needed: </Form.Label>
+                <Form.Control as="select" custom>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                </Form.Control>
             </Form.Group>
+            <div className="cantFind">
+            <p className="cantFindText"> Can't find tool?</p>
+            <Button>add tool</Button>
+            </div>
+            <Form.Group>
+                <Form.Label id="project.materials"> Materials needed: </Form.Label>
+                <Form.Control as="select" custom>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                </Form.Control>
+            </Form.Group>
+            <div className="cantFind">
+            <p className="cantFindText"> Can't find material?</p>
+            <Button>add material</Button>
+            </div>
             <Form.Group>
                 <Form.Label id="project.dateDue"> Date Due: </Form.Label>
                 <Form.Control type="text" placeholder="datepicker? for bootstrap" />
             </Form.Group>
-            <Button> edit </Button>
+            <Button> Save Project </Button>
         </Form>
         </>
     )
