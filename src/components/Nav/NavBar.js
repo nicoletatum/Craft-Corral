e1f71eae073c127e4cbbf6f398b1a92dd5bc99a9
nicoletatum <nicoletatum@outlook.com>
2021-03-18 19:39:46 -0500

    basic css added. getting more comfortable with flexbox

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Jumbotron } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import { authApi, userStorageKey } from "../auth/authSettings"
 import "./NavBar.css"
@@ -8,19 +8,21 @@ import "./NavBar.css"
 export const NavBar = (props) => {
     return (
         <>
+        <Jumbotron className="headBlock">
             <div className="titleblock">
             <h1 className="title"> Crusty's Craft Corral </h1>
             <h3 className="subHead">wranglin' creative endeavors for forty years </h3>
             </div> 
-            <Navbar>
-                    {/* <NavBar.Brand  href="/"> Home </NavBar.Brand> */}
-                    {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-                <Nav className="mr-auto">
+
+            <Navbar className="navBar">
+                <Nav>
+                    <Nav.Link  href="/"> Home </Nav.Link> 
                     <Nav.Link  href="/projects">Projects</Nav.Link>
                     <Nav.Link  href="projects/addProject">Add Project</Nav.Link>
                     <Nav.Link  onClick={e => sessionStorage.clear()} to="/">Sign Out</Nav.Link>    
                 </Nav>
             </Navbar>
+        </Jumbotron>
         </>
     )
 }
